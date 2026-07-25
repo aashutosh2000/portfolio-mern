6fc23184e652555664b0a60aca91c3ebbf1b3bc0
ashu <65132352+aashutosh2000@users.noreply.github.com>
2026-07-26 00:04:38 +0530

    Use Brevo SMTP 465

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,19 +2,17 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 
-  tls: {
-    rejectUnauthorized: false,
-    family: 4,
-  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 const sendEmail = async (name, email, message) => {
